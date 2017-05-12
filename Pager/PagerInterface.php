78f0f11b928f48baca7afc512169f330b6288b3f
core23 <mail@core23.de>
2017-05-12 12:27:12 +0200

    Move public methods to PagerInterface

--- Pager/PagerInterface.php
+++ Pager/PagerInterface.php
@@ -11,6 +11,8 @@
 
 namespace Sonata\DatagridBundle\Pager;
 
+use Sonata\DatagridBundle\ProxyQuery\ProxyQueryInterface;
+
 interface PagerInterface extends \Iterator, \Countable
 {
     /**
@@ -87,4 +89,195 @@ interface PagerInterface extends \Iterator, \Countable
      * @return int
      */
     public function getPreviousPage();
+
+    /**
+     * Returns the current pager's max link.
+     *
+     * @return int
+     */
+    public function getCurrentMaxLink();
+
+    /**
+     * Returns the current pager's max record limit.
+     *
+     * @return int
+     */
+    public function getMaxRecordLimit();
+
+    /**
+     * Sets the current pager's max record limit.
+     *
+     * @param int $limit
+     */
+    public function setMaxRecordLimit($limit);
+
+    /**
+     * Returns an array of page numbers to use in pagination links.
+     *
+     * @param int $nbLinks The maximum number of page numbers to return
+     *
+     * @return array
+     */
+    public function getLinks($nbLinks = null);
+
+    /**
+     * Returns true if the current query requires pagination.
+     *
+     * @return bool
+     */
+    public function haveToPaginate();
+
+    /**
+     * Returns the current cursor.
+     *
+     * @return int
+     */
+    public function getCursor();
+
+    /**
+     * Sets the current cursor.
+     *
+     * @param int $pos
+     */
+    public function setCursor($pos);
+
+    /**
+     * Returns an object by cursor position.
+     *
+     * @param int $pos
+     *
+     * @return mixed
+     */
+    public function getObjectByCursor($pos);
+
+    /**
+     * Returns the current object.
+     *
+     * @return mixed
+     */
+    public function getCurrent();
+
+    /**
+     * Returns the next object.
+     *
+     * @return mixed|null
+     */
+    public function getNext();
+
+    /**
+     * Returns the previous object.
+     *
+     * @return mixed|null
+     */
+    public function getPrevious();
+
+    /**
+     * Returns the first index on the current page.
+     *
+     * @return int
+     */
+    public function getFirstIndice();
+
+    /**
+     * Returns the last index on the current page.
+     *
+     * @return int
+     */
+    public function getLastIndice();
+
+    /**
+     * Returns the number of results.
+     *
+     * @return int
+     */
+    public function getNbResults();
+
+    /**
+     * Returns the maximum number of page numbers.
+     *
+     * @return int
+     */
+    public function getMaxPageLinks();
+
+    /**
+     * Sets the maximum number of page numbers.
+     *
+     * @param int $maxPageLinks
+     */
+    public function setMaxPageLinks($maxPageLinks);
+
+    /**
+     * Returns true if on the first page.
+     *
+     * @return bool
+     */
+    public function isFirstPage();
+
+    /**
+     * Returns true if on the last page.
+     *
+     * @return bool
+     */
+    public function isLastPage();
+
+    /**
+     * Returns the current pager's parameter holder.
+     *
+     * @return array
+     */
+    public function getParameters();
+
+    /**
+     * Returns a parameter.
+     *
+     * @param string $name
+     * @param mixed  $default
+     *
+     * @return mixed
+     */
+    public function getParameter($name, $default = null);
+
+    /**
+     * Checks whether a parameter has been set.
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function hasParameter($name);
+
+    /**
+     * Sets a parameter.
+     *
+     * @param string $name
+     * @param mixed  $value
+     */
+    public function setParameter($name, $value);
+
+    /**
+     * {@inheritdoc}
+     */
+    public function serialize();
+
+    /**
+     * {@inheritdoc}
+     */
+    public function unserialize($serialized);
+
+    /**
+     * @return array
+     */
+    public function getCountColumn();
+
+    /**
+     * @param array $countColumn
+     *
+     * @return array
+     */
+    public function setCountColumn(array $countColumn);
+
+    /**
+     * @return ProxyQueryInterface
+     */
+    public function getQuery();
 }
